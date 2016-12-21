88b36edbaed99bd5aae934879b24a538088c28a5
robertwang <robertwang.tw@gmail.com>
2016-12-21 23:44:35 +0800

    Add all source code

---
+++ remotetime/ExactTime.java
@@ -0,0 +1,13 @@
+/*
+ * File: ./REMOTETIME/EXACTTIME.JAVA
+ * From: .\C15\CORBA\EXACTTIME.IDL
+ * Date: Wed Jun 13 14:04:30 2001
+ *   By: C:\PROGTO~1\JAVA\BIN\IDLTOJ~1.EXE Java IDL 1.2 Aug 18 1998 16:25:34
+ */
+
+package remotetime;
+public interface ExactTime
+    extends org.omg.CORBA.Object, org.omg.CORBA.portable.IDLEntity {
+    String getTime()
+;
+}
